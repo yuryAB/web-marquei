01ff7712c6b80eae9b39db52e04f8d0592085873
yurYab <43591359+yuryAB@users.noreply.github.com>
2026-05-26 00:35:00 -0400

    Update App Store button URL

--- assets/js/config/env.js
+++ assets/js/config/env.js
@@ -1,4 +1,4 @@
 window.MARQUEI_ENV = Object.freeze({
-  APP_STORE_URL: "https://apps.apple.com/br/app/marquei/id6755414706?pt=121157372&ct=site-landingpage&mt=8",
+  APP_STORE_URL: "https://apps.apple.com/br/app/marquei/id6755414706?pt=121157372&ct=instagram-organic-reels-personalizacao&mt=8",
   TESTFLIGHT_URL: "https://testflight.apple.com/join/bp96eu5q",
 });
